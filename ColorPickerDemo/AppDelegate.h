e592767a666d89019327cbbf3b47c3bbd4cf53e7
gzl <gzl@zsbrotherdemac-mini-compile.local>
2016-02-15 16:48:53 +0800

    Initial Commit

---
+++ ColorPickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorPickerDemo
+//
+//  Created by gzl on 16/2/15.
+//  Copyright © 2016年 gzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorPickerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ColorPickerDemo
+//
+//  Created by gzl on 16/2/15.
+//  Copyright © 2016年 gzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
